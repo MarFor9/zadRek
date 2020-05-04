5a2898e7edcd6f640f39c61af827407708640b45
MarFor9 <marfor2@st.amu.edu.pl>
2020-05-04 10:01:03 +0200

    poprawki plus dodanie panelu zarzadczego

--- src/main/java/com/example/zadanierekrutacyjne/ZadanieRekrutacyjneApplication.java
+++ src/main/java/com/example/zadanierekrutacyjne/ZadanieRekrutacyjneApplication.java
@@ -1,7 +1,14 @@
 package com.example.zadanierekrutacyjne;
 
+import com.sun.faces.config.ConfigureListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import javax.faces.webapp.FacesServlet;
 
 @SpringBootApplication
 public class ZadanieRekrutacyjneApplication {
@@ -10,4 +17,23 @@ public class ZadanieRekrutacyjneApplication {
         SpringApplication.run(ZadanieRekrutacyjneApplication.class, args);
     }
 
+    @Bean
+    public ServletRegistrationBean facesServletRegistration() {
+        ServletRegistrationBean registration = new ServletRegistrationBean<>(new FacesServlet(), "*.xhtml");
+        registration.setLoadOnStartup(1);
+        return registration;
+    }
+
+    @Bean
+    public ServletContextInitializer servletContextInitializer() {
+        return servletContext -> {
+            servletContext.setInitParameter("com.sun.faces.forceLoadConfiguration", Boolean.TRUE.toString());
+            servletContext.setInitParameter("primefaces.THEME", "nova-light");
+        };
+    }
+
+    @Bean
+    public ServletListenerRegistrationBean<ConfigureListener> jsfConfigureListener() {
+        return new ServletListenerRegistrationBean<>(new ConfigureListener());
+    }
 }
